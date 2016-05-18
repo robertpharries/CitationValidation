7f657475d0035ec783b6ca537ce436397507c016
auVeypor <lm671@uowmail.edu.au>
2016-05-18 16:50:02 +1000

    Updated Main for testing

--- main.cpp
+++ main.cpp
@@ -5,34 +5,13 @@ using namespace std;
 
 int main()
 {
-	//read DOI input from file using doiIO
+
 	doiIO input;
-	//Create reference structs as a vector/array based on input.count()
-
-	getFromApi("10.1049/iet-sen.2009.0084");
-
 	for(int i = 0; i < input.count(); i++)
 	{
 		string doi = input.next();
-
-		//query API -> scopus
-		//process the resulting API json
-		//Place into an array/vector of structs
-		bool processed = false;
-		while(!processed)
-		{
-			corRef corrRef;
-			//Call process api object, pass it the reference struct
-			//Check for a match in scopus API until a match is found or not match can be identified (Process Results Class)
-			//Compare returned DOI and add corrected references in the correctedRef struct
-		}
-
-		//Call output function to create the csv file and output the reference struct and corrRef
-		//Will need to be passed both structs according to the current input.count()
+		getFromApi(doi);
 	}
-
-
-
 
 	return 0;
 }
